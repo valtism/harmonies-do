d1f63333f799ed59680f27721ab2e7ecea03659f
Dan Wood <daniel4wood@icloud.com>
2025-10-10 22:11:47 +1100

    Add worker config

--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,30 @@
-export default {
-  fetch(request) {
-    const url = new URL(request.url);
+import { DurableObject } from "cloudflare:workers";
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
-    }
-		return new Response(null, { status: 404 });
+export interface Env {
+  HARMONIES: DurableObjectNamespace<Harmonies>;
+}
+
+// Worker
+export default {
+  async fetch(request, env) {
+    // A stub is a client used to invoke methods on the Durable Object
+    console.log(env);
+    const stub = env.HARMONIES.getByName("foo");
+
+    // Methods on the Durable Object are invoked via the stub
+    const rpcResponse = await stub.sayHello();
+
+    return new Response(rpcResponse);
   },
 } satisfies ExportedHandler<Env>;
+
+// Durable Object
+export class Harmonies extends DurableObject {
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env);
+  }
+
+  async sayHello(): Promise<string> {
+    return "Hello, World!";
+  }
+}
